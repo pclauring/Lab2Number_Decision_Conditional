495aa937d27f001549b2faba2d2ed822d390af1f
Pierce C Lauring <pclauring@gmail.com>
2017-10-25 15:37:11 -0400

    first commit

--- Lab2_Decision_Conditional/Program.cs
+++ Lab2_Decision_Conditional/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab2_Decision_Conditional
 {
@@ -10,6 +6,65 @@ namespace Lab2_Decision_Conditional
     {
         static void Main(string[] args)
         {
+            bool repeat = true;
+            do
+            {
+                Console.Write("Please enter an integer from (1-100): ");
+
+                string oddOrEven = Console.ReadLine();
+                int.TryParse(oddOrEven, out int TestInt);
+
+                Console.Write("Output: ");
+
+                if ((TestInt % 2) == 0)
+                {
+                    if (TestInt >= 2 && TestInt <= 25)
+                    {
+                        Console.WriteLine("The integer is even and less than 25.");
+                    }
+                    else if (TestInt >= 26 && TestInt <= 60)
+                    {
+                        Console.WriteLine("Even.");
+                    }
+                    else if (TestInt > 60)
+                    {
+                        Console.WriteLine("{0}: Even.", TestInt);
+                    }
+                }
+                else if (TestInt > 60)
+                {
+                    Console.WriteLine("{0}: Odd.", TestInt);
+                }
+                else
+                {
+                    Console.WriteLine("Odd.");
+                }
+
+                Console.WriteLine("Would you like to repeat (y/n)? :");
+
+                bool validAnswer = true;
+                while (validAnswer)
+                {
+                    string repeatPrompt = Console.ReadLine();
+                    if (repeatPrompt == "y" || repeatPrompt == "Y" || repeatPrompt == "yes")
+                    {
+                        repeat = true;
+                        validAnswer = false;
+                    }
+                    else if (repeatPrompt == "n" || repeatPrompt == "N" || repeatPrompt == "no")
+                    {
+                        Console.WriteLine("Okay exiting the program now.");
+                        repeat = false;
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Didn't enter a valid answer. Would you like to test another number (y/n)?: ");
+                        validAnswer = true;
+                    }
+                }
+
+            } while (repeat);
         }
     }
 }
